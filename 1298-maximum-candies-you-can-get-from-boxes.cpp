13fe55f7f31b657a3429b3540bf282e3c8a03a3a
Benjamin Wu <soggysocks206@gmail.com>
2025-06-15 23:02:28 -0700

    Time: 14 ms (12.94%), Space: 45.62 MB (7.39%)

---
+++ 1298-maximum-candies-you-can-get-from-boxes.cpp
@@ -0,0 +1,53 @@
+#include <vector>
+#include <deque>
+using namespace std;
+
+class Solution {
+public:
+    int maxCandies(vector<int>& status,
+                   vector<int>& candies,
+                   vector<vector<int>>& keys,
+                   vector<vector<int>>& containedBoxes,
+                   vector<int>& initialBoxes) {
+        std::deque<int> ready_boxes;
+        int *is_reachable = new int[status.size()];
+        int *visited = new int[status.size()];
+
+        for (int i = 0; i < status.size(); ++i) {
+            is_reachable[i] = 0;
+            visited[i] = 0;
+        }
+
+        for (int i = 0; i < initialBoxes.size(); ++i) {
+            if (status[initialBoxes[i]] == 1) {
+                ready_boxes.emplace_back(initialBoxes[i]);
+                visited[initialBoxes[i]] = 1;
+            }
+            is_reachable[initialBoxes[i]] = 1;
+        }
+
+        int candy = 0;
+        while (ready_boxes.size() > 0) {
+            int box = ready_boxes.front();
+            ready_boxes.pop_front();
+            candy += candies[box];
+            for (int i = 0; i < keys[box].size(); ++i) {
+                int key = keys[box][i];
+                status[key] = 1;
+            }
+            for (int i = 0; i < containedBoxes[box].size(); ++i) {
+                int contained_box = containedBoxes[box][i];
+                is_reachable[contained_box] = 1;
+            }
+            for (int i = 0; i < status.size(); ++i) {
+                if (status[i] == 1 && is_reachable[i] == 1 && visited[i] == 0) {
+                    ready_boxes.emplace_back(i);
+                    visited[i] = 1;
+                }
+            }
+        }
+        delete[] is_reachable;
+        delete[] visited;
+        return candy;
+    }
+};
