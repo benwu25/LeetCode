6abeef4cfbb2ccb9736902fb8b0f9862363d771d
Benjamin Wu <soggysocks206@gmail.com>
2025-06-23 20:26:49 -0700

    Time: 0 ms (100%), Space: 12.65 MB (80.83%)

--- 120-triangle.cpp
+++ 120-triangle.cpp
@@ -4,25 +4,22 @@ using namespace std;
 class Solution {
 public:
     int minimumTotal(vector<vector<int>>& triangle) {
-        int **best = new int*[triangle.size()];
-        for (int i = 0; i < triangle.size(); ++i) {
-            best[i] = new int[triangle[triangle.size() - 1].size()];
-        }
-
-        for (int elem = 0; elem < triangle[triangle.size() - 1].size(); ++elem) {
-            best[triangle.size() - 1][elem] = triangle[triangle.size() - 1][elem];
+        int N = triangle[triangle.size()-1].size();
+        int *best_prev_row = new int[N];
+        for (int elem = 0; elem < N; ++elem) {
+            best_prev_row[elem] = triangle[triangle.size()-1][elem];
         }
 
         for (int row = triangle.size() - 2; row >= 0; --row) {
             for (int elem = 0; elem < triangle[row].size(); ++elem) {
-                int nxt_rw = row + 1;
-                if (best[nxt_rw][elem] < best[nxt_rw][elem+1]) {
-                    best[row][elem] = triangle[row][elem] + best[nxt_rw][elem];
+                if (best_prev_row[elem] < best_prev_row[elem + 1]) {
+                    best_prev_row[elem] = triangle[row][elem] + best_prev_row[elem];
                 } else {
-                    best[row][elem] = triangle[row][elem] + best[nxt_rw][elem+1];
+                    best_prev_row[elem] = triangle[row][elem] + best_prev_row[elem+1];
                 }
+
             }
         }
-        return best[0][0];
+        return best_prev_row[0];
     }
 };
