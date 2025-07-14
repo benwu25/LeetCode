5eded40666b3a5fc6f836a3fe6e82de9ccbf02f4
Ben Wu <bwu25@cs.washington.edu>
2025-07-13 23:19:34 -0700

    Time: 3 ms (70.31%), Space: 101.93 MB (84.56%)

--- 463-island-perimeter.cpp
+++ 463-island-perimeter.cpp
@@ -6,45 +6,25 @@ class Solution {
 public:
 
     int islandPerimeter(vector<vector<int>>& grid) {
+        int perimeter = 0;
         for (int i = 0; i < grid.size(); ++i) {
             for (int j = 0; j < grid[0].size(); ++j) {
                 if (grid[i][j]) {
-                    int perimeter = 0;
-                    deque<pair<int, int>> ready;
-                    ready.push_back(pair<int, int>(i, j));
-                    grid[i][j] = -1;
-                    while (ready.size()) {
-                        pair<int, int> next = ready.back();
-                        ready.pop_back();
-                        if (next.first-1 >= 0 && grid[next.first-1][next.second] == 1) {
-                            ready.push_back(pair<int, int>(next.first-1, next.second));
-                            grid[next.first-1][next.second] = -1;
-                        } else if (next.first-1 < 0 || grid[next.first-1][next.second] == 0) {
-                            ++perimeter;
-                        }
-                        if (next.second+1 < grid[0].size() && grid[next.first][next.second+1] == 1) {
-                            ready.push_back(pair<int, int>(next.first, next.second+1));
-                            grid[next.first][next.second+1] = -1;
-                        } else if (next.second+1 >= grid[0].size() || grid[next.first][next.second+1] == 0) {
-                            ++perimeter;
-                        }
-                        if (next.first+1 < grid.size() && grid[next.first+1][next.second] == 1) {
-                            ready.push_back(pair<int, int>(next.first+1, next.second));
-                            grid[next.first+1][next.second] = -1;
-                        } else if (next.first+1 >= grid.size() || grid[next.first+1][next.second] == 0) {
-                            ++perimeter;
-                        }
-                        if (next.second-1 >= 0 && grid[next.first][next.second-1] == 1) {
-                            ready.push_back(pair<int, int>(next.first, next.second-1));
-                            grid[next.first][next.second-1] = -1;
-                        } else if (next.second-1 < 0 || grid[next.first][next.second-1] == 0) {
-                            ++perimeter;
-                        }
+                    if (j-1 < 0 || !grid[i][j-1]) {
+                        ++perimeter;
                     }
-                    return perimeter;
+                    if (i-1 < 0 || !grid[i-1][j]) {
+                        ++perimeter;
+                    }
+                    if (j+1 >= grid[0].size() || !grid[i][j+1]) {
+                        ++perimeter;
+                    }
+                    if (i+1 >= grid.size() || !grid[i+1][j]) {
+                        ++perimeter;
+                    }
                 }
             }
         }
-        return -1;
+        return perimeter;
     }
 };
