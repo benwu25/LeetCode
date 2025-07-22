71970578bab4d8c40d1cf1183a738d0cb8c7102d
Ben Wu <bwu25@cs.washington.edu>
2025-07-21 23:36:08 -0700

    Time: 4 ms (86.56%), Space: 27.60 MB (90.99%)

--- 221-maximal-square.cpp
+++ 221-maximal-square.cpp
@@ -1,33 +1,44 @@
+#include <vector>
+using namespace std;
+
 class Solution {
 public:
     int maximalSquare(vector<vector<char>>& matrix) {
         int max = 0;
-        int is_valid[matrix.size()][matrix[0].size()];
-        for (int i = 0; i < matrix.size(); ++i) {
-            for (int j = 0; j < matrix[0].size(); ++j) {
-                if (matrix[i][j] == '1') {
-                    max = 1;
-                    is_valid[i][j] = 1;
-                } else {
-                    is_valid[i][j] = 0;
-                }
-            }
+        int best[matrix.size()][matrix[0].size()];
+        for (int j = 0; j < matrix[0].size(); ++j) {
+            if (matrix[0][j] == '1')
+                best[0][j] = max = 1;
+            else
+                best[0][j] = 0;
         }
-        int mn = (matrix.size() < matrix[0].size()) ? matrix.size() : matrix[0].size();
-        for (int size = 2; size <= mn; ++size) {
-            for (int i = 0; i+size <= matrix.size(); ++i) {
-                for (int j = 0; j+size <= matrix[0].size(); ++j) {
-                    if (is_valid[i][j] && is_valid[i+1][j]
-                    && is_valid[i][j+1] && is_valid[i+1][j+1]) {
-                        is_valid[i][j] = 1;
-                        max = size;
-                    } else
-                        is_valid[i][j] = 0;
+        for (int i = 0; i < matrix.size(); ++i) {
+            if (matrix[i][0] == '1')
+                best[i][0] = max = 1;
+            else
+                best[i][0] = 0;
+        }
+        for (int i = 1; i < matrix.size(); ++i) {
+            for (int j = 1; j < matrix[0].size(); ++j) {
+                if (matrix[i][j] == '0') {
+                    best[i][j] = 0;
+                } else if (best[i][j-1] > 0 && best[i-1][j] > 0 && best[i-1][j-1] > 0) {
+                    best[i][j] = 1+std::min(best[i][j-1], std::min(best[i-1][j], best[i-1][j-1]));
+                } else {
+                    best[i][j] = 1;
                 }
+                max = std::max(max, best[i][j]);
             }
         }
         return max * max;
     }
 };
 
-// optimize: need to go bottom up and build on top of one of the corners, like a square prefix sum
+/*
+
+let best[i][j] give length of longest square with bottom right corner at i,j
+best[i][j] = 0 if matrix[i][j] == 0
+otherwise if best[i][j-1] > 0 and best[i-1][j] > 0 and best[i-1][j-1] > 0, best[i][j] = 1+min
+otherwise best[i][j] = 1
+
+*/
