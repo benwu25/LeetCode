02531e8bba7390c16fa59fa754fadb144dc59c09
Ben Wu <bwu25@cs.washington.edu>
2025-07-27 14:23:32 -0700

    Time: 0 ms (100%), Space: 19.80 MB (5.68%)

--- 21-merge-two-sorted-lists.cpp
+++ 21-merge-two-sorted-lists.cpp
@@ -1,58 +1,39 @@
-/**
- * Definition for singly-linked list.
- */
+
+// Definition for singly-linked list.
 struct ListNode {
-     int val;
-     ListNode *next;
-     ListNode() : val(0), next(nullptr) {}
-     ListNode(int x) : val(x), next(nullptr) {}
-     ListNode(int x, ListNode *next) : val(x), next(next) {}
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
 };
 
 class Solution {
 public:
     ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
-        if (list1 == nullptr && list2 == nullptr) {
-            return nullptr;
-        } else if (list1 == nullptr) {
+        if (!list1)
             return list2;
-        } else if (list2 == nullptr) {
+        if (!list2)
             return list1;
-        }
-        ListNode *front;
-        if (list1->val < list2->val) {
-            front = list1;
+        ListNode *res;
+        if (list1->val <= list2->val) {
+            res = new ListNode(list1->val);
             list1 = list1->next;
         } else {
-            front = list2;
+            res = new ListNode(list2->val);
             list2 = list2->next;
         }
-        front->next = nullptr;
-        ListNode *back = front;
-        while (1) {
-            if (list1 == nullptr && list2 == nullptr) {
-                break;
-            } else if (list1 == nullptr) {
-                back->next = list2;
+        ListNode *back = res;
+        while (list1 || list2) {
+            if (!list1 || list2 && list2->val <= list1->val) {
+                back->next = new ListNode(list2->val);
                 list2 = list2->next;
-                back = back->next;
-            } else if (list2 == nullptr) {
-                back->next = list1;
+            } else if (!list2 || list1 && list1->val <= list2->val) {
+                back->next = new ListNode(list1->val);
                 list1 = list1->next;
-                back = back->next;
-            } else {
-                if (list1->val < list2->val) {
-                    back->next = list1;
-                    list1 = list1->next;
-                    back = back->next;
-                } else {
-                    back->next = list2;
-                    list2 = list2->next;
-                    back = back->next;
-                }
             }
-            back->next = nullptr;
+            back = back->next;
         }
-        return front;
+        return res;
     }
 };
