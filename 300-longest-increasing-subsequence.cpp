7dc2f356d784fe640101de28d53d1805deeb1195
benwu25 <bwu25@cs.washington.edu>
2025-10-22 15:02:42 -0700

    Time: 39 ms (75.47%), Space: 14.42 MB (49.13%)

--- 300-longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence.cpp
@@ -4,21 +4,18 @@ using namespace std;
 class Solution {
 public:
     int lengthOfLIS(vector<int>& nums) {
-        int *best = new int[nums.size()];
+        vector<int> best(nums.size());
+        int longest = 1;
         for (int i = 0; i < nums.size(); ++i) {
             best[i] = 1;
             for (int j = 0; j < i; ++j) {
-                if (nums[j] < nums[i] && 1+best[j] > best[i]) {
-                    best[i] = 1 + best[j];
-                }
+                if (nums[i] > nums[j])
+                    best[i] = std::max(best[i], 1 + best[j]);
             }
+
+            longest = std::max(longest, best[i]);
         }
-        int max = -1;
-        for (int i = 0; i < nums.size(); ++i) {
-            if (best[i] > max) {
-                max = best[i];
-            }
-        }
-        return max;
+
+        return longest;
     }
 };
